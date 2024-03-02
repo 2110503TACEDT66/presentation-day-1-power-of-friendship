b45a0fa320242d6c28cb03e95b35b6c4abf81c60
WanRatchapol <93596858+WanRatchapol@users.noreply.github.com>
2024-03-02 17:19:38 +0700

    Organize controllers/companies.js file

--- controllers/companies.js
+++ controllers/companies.js
@@ -1,138 +1,152 @@
-const Appointment = require("../models/Appointment");
 const Company = require("../models/Company");
-const vacCenter = require("../models/VacCenter");
 
-exports.getCompanies = async (req,res,next)=>{
+//@desc   Get all companies
+//@route  GET /api/v1/companies
+//@access Public
+exports.getCompanies = async (req, res, next) => {
     let query;
 
     //Copy req.query
     const reqQuery = {...req.query};
 
     //Fields to exclude
-    const removeFields = ['select','sort','page','limit'];
+    const removeFields = ['select', 'sort', 'page', 'limit'];
 
     //Loop over remove fields and delete them from reqQuery
-    removeFields.forEach(param=>delete reqQuery[param]);
+    removeFields.forEach(param => delete reqQuery[param]);
     console.log(reqQuery);
 
     //Create query string
     let queryStr = JSON.stringify(reqQuery);
-    queryStr=queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g,match=> `$${match}`);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
     query = Company.find(JSON.parse(queryStr)).populate('appointments');
 
     //Select Fields
-    if(req.query.select){
+    if (req.query.select) {
         const fields = req.query.select.split(',').join(' ');
-        query=query.select(fields);
+        query = query.select(fields);
     }
 
     //Sort
-    if(req.query.sort){
+    if (req.query.sort) {
         const sortBy = req.query.sort.split(',').join(' ');
         query = query.sort(sortBy);
-    }else{
-        query = query.sort("name");
+    } else {
+        query = query.sort('name');
     }
     
     //Pagination
-    const page = parseInt(req.query.page,10)||1;
-    const limit = parseInt(req.query.limit,10)||25;
-    const startIndex = (page-1)*limit;
-    const endIndex= page*limit;
+    const page = parseInt(req.query.page,10) || 1;
+    const limit = parseInt(req.query.limit,10) || 25;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
     
-
-    try{
+    try {
         const total = await Company.countDocuments();
-        query=query.skip(startIndex).limit(limit);
+
+        query = query.skip(startIndex).limit(limit);
 
         //Execute query
         const companies = await query;
 
         //Pagination result
-        const pagination={};
+        const pagination = {};
 
-        if(endIndex<total){
-            pagination.next={
-                page:page+1,
+        if (endIndex < total) {
+            pagination.next = {
+                page: page + 1,
                 limit
             }
         }
 
-        if(startIndex>0){
-            pagination.prev={
-                page:page-1,
+        if(startIndex > 0) {
+            pagination.prev = {
+                page: page - 1,
                 limit
             }
         }
 
-        res.status(200).json({success:true, count: companies.length, data:companies});
-    }catch(err){
-        res.status(400).json({success:false});
+        res.status(200).json({
+            success: true, 
+            count: companies.length, 
+            data: companies
+        });
+    } catch (err) {
+        res.status(400).json({success: false});
     }
 }
 
-exports.getCompany = async(req,res,next)=>{
-    try{
+//@desc   Get single company
+//@route  GET /api/v1/companies/:id
+//@access Public
+exports.getCompany = async (req, res, next) => {
+    try {
         const company = await Company.findById(req.params.id);
-        if(!company){
-            return res.status(400).json({succes:false});
+
+        if (!company) {
+            return res.status(400).json({succes: false});
         }
-        res.status(200).json({success:true,data:company});
-    }catch(err){
-        res.status(400).json({success:false});
+        res.status(200).json({
+            success: true,
+            data: company});
+    } catch (err) {
+        res.status(400).json({success: false});
     }
 }
 
-exports.createCompany =  async(req,res,next)=>{
-    const company =await Company.create(req.body);
+//@desc   Create a company
+//@route  POST /api/v1/companies
+//@access Private
+exports.createCompany =  async (req, res, next) => {
+    const company = await Company.create(req.body);
     res.status(201).json({
         succes: true,
         data:company
     });
 }
 
-exports.updateCompany = async(req,res,next)=>{
-    try{
-        const company = await Company.findByIdAndUpdate(req.params.id,req.body,{
+//@desc   Update single company
+//@route  PUT /api/v1/companies/:id
+//@access Private
+exports.updateCompany = async (req, res, next) => {
+    try {
+        const company = await Company.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
             runValidators: true
         });
-        if(!company){
+
+        if (!company) {
             return res.status(400).json({success: false});
         }
-        res.status(200).json({success:true,data: company});
-    }catch(err){
-        res.status(400).json({success:false});
+
+        res.status(200).json({
+            success: true,
+            data: company
+        });
+    } catch (err) {
+        res.status(400).json({success: false});
     }
 }
 
-exports.deleteCompany = async (req,res,next)=>{
-    try{
+//@desc   Delete single company
+//@route  DELETE /api/v1/companies/:id
+//@access Private
+exports.deleteCompany = async (req, res, next) => {
+    try {
         const company = await Company.findById(req.params.id);
 
-        if(!company){
+        if (!company) {
             return res.status(400).json({success: false});
         }
+
         await company.deleteOne();
-        res.status(200).json({success:true,data: {}});
-    }catch(err){
-        res.status(400).json({success:false});
+
+        res.status(200).json({
+            success: true,
+            data: {}
+        });
+    } catch (err) {
+        res.status(400).json({success: false});
     }
 }
-
-exports.getVacCenters = (req,res,next)=>{
-    vacCenter.getAll((err,data)=>{
-        if(err){
-            res.status(500).send({
-                message: err.message || "Some error occurred while retrieving Vaccine Centers."
-            });
-        }else{
-            res.send(data);
-        };
-    });
-};
-
-
-
-
